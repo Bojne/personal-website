a1605d84ab7d08c4a29d4ff2ae24370b63240b19
bojne <bojne.john@gmail.com>
2019-11-26 22:15:54 +0900

    Finished && gp

--- dist/mysite.e31bb0bc.js
+++ dist/mysite.e31bb0bc.js
@@ -31916,7 +31916,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54170" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58625" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
